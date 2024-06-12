77161ae87d3480d56e3eb36ba32c7f827a3f1f01
Manuel Favagrossa <159720066+ZioTogni@users.noreply.github.com>
2024-06-12 20:00:23 +0200

    Add files via upload

---
+++ Calcolatricedigradi.py
@@ -0,0 +1,23 @@
+#VARIABILI
+nome_aiutante="Calcolatrice di gradi"
+celsius_kelvin="da Celsius a Kelvin"
+kelvin_celsius="da Kelvin a Celsius"
+#PROGRAMMA
+print("Salve, io sono la "+nome_aiutante+"!")
+print("Sono qui per aiutarti a calcolare i gradi "+celsius_kelvin+" e "+kelvin_celsius+".")
+print("\nPerfavore, scegli il servizio che vuoi!")
+servizio_richiesto=input()
+if servizio_richiesto==celsius_kelvin:
+    print("Perfetto, hai scelto il servizio"+servizio_richiesto+" ! ")
+    x=input("Inserisci il numero da convertire in Kelvin!")
+    s=input("Inserisci 273.15! (per convertire il tuo numero in Kelvin)")
+    a=float(x)+float(s)
+    print("Il numero che vuoi convertire in Kelvin risulta",a)
+elif servizio_richiesto==kelvin_celsius:
+    print("Perfetto, hai scelto il servizio "+servizio_richiesto+" ! ")
+    x=input("Inserisci il numero da convertire in Celsius!")
+    s=input("Inserisci 273.15 (per convertire il tuo numero in Celsius)")
+    a=float(x)-float(s)
+    print("Il numero che vuoi convertire in Celsius risulta",a)
+else: 
+    print("Non sono ancora capace a calcolare il servizio richiesto, perfavore riavvia il programma!")
